5a18f441e564da9deeb377752c05701542302e6a
mraees93 <53144654+mraees93@users.noreply.github.com>
2024-09-13 15:09:53 +0200

    setup environment

---
+++ backend/Data/AppDBContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+    }
+}
