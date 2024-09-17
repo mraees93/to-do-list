99d9348297db737e001d10f83e751be7fbcbc8a0
mraees93 <53144654+mraees93@users.noreply.github.com>
2024-09-17 11:13:38 +0200

    added env variables for db

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,15 +1,16 @@
-using backend.Data;
+using backend.database;
 using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+
+DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddScoped<ApplicationDbContext>();
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnectionMySQL"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnectionMySQL"))));
 
-builder.Services
-    .AddDbContext<AppDBContext>(options=>options
-    .UseSqlServer(builder.Configuration
-    .GetConnectionString("DefaultConnection")));
-    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a18f441e564da9deeb377752c05701542302e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
